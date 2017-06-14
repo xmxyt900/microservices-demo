33d20679ff4dbf5f8ebf103d52592fd729bf34e5
xmxyt900 <xmxyt900@gmail.com>
2017-06-14 01:50:10 +0000

    new recommendation start/stop files, compose file

--- BrownoutPrototype/src/startRecommendationEngine.sh
+++ BrownoutPrototype/src/startRecommendationEngine.sh
@@ -1,4 +1,5 @@
 
-#!/bin/bash
-sudo docker run -ti -d --name=recommendation_engine -v DataVolume:/DataVolume ubuntu
-sudo docker exec recommendation_engine /bin/sh -c "echo 'This file is shared between containers' > /DataVolume/data.txt; apt-get update; apt-get install stress; stress --cpu 1 --timeout 120"
+
+#!/bin/sh
+ansible DockerCluster1 -m shell -a 'sh startRecommendationEngine.sh'
+ansible DockerCluster2 -m shell -a 'sh startRecommendationEngine.sh'
--- BrownoutPrototype/src/stopRecommendationEngine.sh
+++ BrownoutPrototype/src/stopRecommendationEngine.sh
@@ -1,5 +1,5 @@
 
-#!/bin/bash
-sudo docker stop recommendation_engine
-sudo docker rm recommendation_engine
-sudo docker volume rm DataVolume
+
+#!/bin/sh
+ansible DockerCluster1 -m shell -a 'sh stopRecommendationEngine.sh'
+ansible DockerCluster2 -m shell -a 'sh stopRecommendationEngine.sh'
